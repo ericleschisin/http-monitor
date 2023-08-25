58be135d14e426f42ec750b658fc92066f998028
Eric Leschisin <eric.leschisin@gmail.com>
2023-08-24 22:16:23 -0500

    FIX: correct output to only store root domain urls

--- src/monitor.py
+++ src/monitor.py
@@ -3,6 +3,7 @@ import os.path
 import yaml
 import requests
 import time
+from urllib.parse import urlparse
 from datetime import timedelta
 
 MAX_LATENCY = timedelta(milliseconds=500)
@@ -23,20 +24,21 @@ except NoSuchFileException:
   print(e)
 
 
-def update_history(url,r):
+def output_history():
+  for url in history:
+    uptime_percentage = 100 * (history[url]['num_up'] / history[url]['num_checked'])
+    print("{0} has {1} % availability percentage".
+      format(url, uptime_percentage))
+
+def update_history(url,response):
   if url in history:
     entry = history[url]
   else:
     entry = { 'num_checked': 0 ,'num_up': 0 }
-
   entry['num_checked'] += 1
-  if 200 <= r.status_code <= 299 and r.elapsed < MAX_LATENCY:
+  if 200 <= response.status_code <= 299 and response.elapsed < MAX_LATENCY:
     entry['num_up'] += 1
-
-  history.update({ url : entry})
-  uptime_percentage = 100 * (entry['num_up'] / entry['num_checked'])
-  print("{0} has {1} % availability percentage".
-    format(url, uptime_percentage))
+  history.update({ url : entry })
 
 
 def check_endpoint(endpoint):
@@ -44,25 +46,23 @@ def check_endpoint(endpoint):
     r_headers = endpoint['headers']
   else:
     r_headers = {'user-agent': 'fetch-synthetic-monitor'}
-
   # Send POST or Get request
   if 'method' in endpoint.keys() and endpoint['method'] == 'POST':
     if 'body' in endpoint.keys():
       r_data = endpoint['body']
     else:
       r_data = {'body': ''}
-
     r = requests.post(endpoint['url'], data=r_data, headers=r_headers)
   else:
     r = requests.get(endpoint['url'], headers=r_headers)
-
-  update_history(endpoint['url'],r)
+  update_history(urlparse(endpoint['url']).netloc,r)
 
 
 if __name__ == "__main__":
   while(True):
     for endpoint in config:
       check_endpoint(endpoint)
-    time.sleep(INTERVAL)
 
+    output_history()
+    time.sleep(INTERVAL)
 
